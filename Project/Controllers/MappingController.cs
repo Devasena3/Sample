eee2e28b5fdeb07bfa62ba4900e6551793b88778
Vinitha A <vinitha.am@Unite.care>
2023-02-04 16:38:01 +0530

    Init Project

---
+++ Project/Controllers/MappingController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Controllers
+{
+  public class MappingController : Controller
+  {
+    // GET: MappingController
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    // GET: MappingController/Details/5
+    public ActionResult Details(int id)
+    {
+      return View();
+    }
+
+    // GET: MappingController/Create
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    // POST: MappingController/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create(IFormCollection collection)
+    {
+      try
+      {
+        return RedirectToAction(nameof(Index));
+      }
+      catch
+      {
+        return View();
+      }
+    }
+
+    // GET: MappingController/Edit/5
+    public ActionResult Edit(int id)
+    {
+      return View();
+    }
+
+    // POST: MappingController/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit(int id, IFormCollection collection)
+    {
+      try
+      {
+        return RedirectToAction(nameof(Index));
+      }
+      catch
+      {
+        return View();
+      }
+    }
+
+    // GET: MappingController/Delete/5
+    public ActionResult Delete(int id)
+    {
+      return View();
+    }
+
+    // POST: MappingController/Delete/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Delete(int id, IFormCollection collection)
+    {
+      try
+      {
+        return RedirectToAction(nameof(Index));
+      }
+      catch
+      {
+        return View();
+      }
+    }
+  }
+}
---
+++ Project/Models/AppDbContext.cs
@@ -0,0 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Project.Models
+{
+  public class AppDbContext:DbContext
+  {
+  }
+}
